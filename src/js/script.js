b11bf377d5c5e1acf294da5d972ba407a2c3b0b8
Katya Fadeeva <katfadmail@gmail.com>
2018-11-05 21:11:20 +0200

    had added function for draw squares

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,37 @@
-let canvas = document.getElementById("squares");
-let ctx = canvas.getContext("2d");
+window.onload = function() { init(); }
+
+let canvas = null;
+let ctx = null;
+
+function init() {
+    canvas = document.getElementById("squares");
+    ctx = canvas.getContext("2d");
+    
+    let squareColor = {
+        dark : '#987a64',
+        light : '#e4dad3'
+    }
+    
+    let sideOfSquare = 80;
+    
+    drawChessSquares(sideOfSquare, squareColor);
+}
+
+function drawChessSquares(sideOfSquare, squareColor) {
+    let currentColor = squareColor.light;
+    let x = 0, y = 0; 
+    for(let i = 0; i < 8; i++) {
+        for(let j = 0; j < 8; j++) {
+            ctx.fillStyle = currentColor;
+            ctx.fillRect(x, y, sideOfSquare, sideOfSquare);
+            ctx.fill();
+            x += 80;
+
+            if(j != 7)
+                currentColor = (currentColor == squareColor.light) ? squareColor.dark : squareColor.light;
+        }
+        x = 0;
+        y += sideOfSquare;   
+    }
+
+}
