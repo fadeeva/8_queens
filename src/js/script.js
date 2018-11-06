04e10928519e6333b81734467694b8f87ba3a5ad
Katya Fadeeva <katfadmail@gmail.com>
2018-11-06 23:10:33 +0200

    center of chess square

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,8 @@ let ctx = null;
 
 function init() {
     canvas = document.getElementById("squares");
+    canvasCnt = document.getElementById("chess_desk");
+    
     ctx = canvas.getContext("2d");
     
     let squareColor = {
@@ -15,6 +17,38 @@ function init() {
     let sideOfSquare = 80;
     
     drawChessSquares(sideOfSquare, squareColor);
+    
+    
+    canvas.addEventListener("click", function(event) {
+        let mousePos = getClickCoord(canvas, canvasCnt, event)
+        drawDot(mousePos.x, mousePos.y, sideOfSquare)
+    });
+
+}
+
+function drawDot(x, y, sideOfSquare) {
+    let centerX = Math.ceil(x / sideOfSquare) * sideOfSquare - sideOfSquare / 2;
+    let centerY = Math.ceil(y / sideOfSquare) * sideOfSquare - sideOfSquare / 2;
+    
+    console.log(centerX + ", " + centerY + " | " + x + ", " + y);
+    
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, 10, 0, 2 * Math.PI, false);
+    ctx.fillStyle = "#232323";
+    ctx.fill();
+}
+
+function getClickCoord(canvas, canvasCnt, event) {
+    
+    let offsetX = canvas.offsetLeft;
+    let offsetY = canvas.offsetTop;
+    let offsetCntY = canvasCnt.offsetTop;
+    let offsetCntX = canvasCnt.offsetLeft;
+    
+    let mouseX = Math.abs(parseInt(event.pageX-(offsetX + offsetCntX)));
+    let mouseY = Math.abs(parseInt(event.pageY-(offsetY + offsetCntY)));
+    
+    return { x: mouseX, y: mouseY }
 }
 
 function drawChessSquares(sideOfSquare, squareColor) {
